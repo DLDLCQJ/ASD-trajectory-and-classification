257fdd901f9aa32c7cb4dc61e50fe6503132c152
Simon_Wang <145650040+DLDLCQJ@users.noreply.github.com>
2023-11-22 11:52:22 +0800

    Create create_graph.py

---
+++ utils/create_graph.py
@@ -0,0 +1,8 @@
+def create_graph(dist, parameter, self_dist=True):
+    if self_dist:
+        assert dist.shape[0] == dist.shape[1], "Input must be a square matrix for self distances."
+    graph = (dist <= parameter).float()
+    if self_dist:
+        # Avoid self loops in the graph by setting diagonal elements to 0
+        np.fill_diagonal(graph.numpy(), 0)
+    return graph
