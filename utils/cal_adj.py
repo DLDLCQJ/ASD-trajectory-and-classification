2fefabfb6aefe26ed5c3fbec17543ba5979d3db3
Simon_Wang <simon971234@gmail.com>
2024-05-10 15:17:20 +0800

    Update cal_adj.py

--- utils/cal_adj.py
+++ utils/cal_adj.py
@@ -1,3 +1,23 @@
+from utils.create_graph import *
+from utils.cosine_dis import *
+
+def to_sparse(x):
+    # Check if the tensor is already sparse
+    if x.is_sparse:
+        return x
+    # Find non-zero indices
+    indices = torch.nonzero(x, as_tuple=False)
+    # Handle the case where all elements are zeros
+    if indices.numel() == 0:
+        return torch.sparse_coo_tensor([], [], x.size()).to(x.dtype)
+
+    # Transpose the indices to match sparse tensor format
+    indices = indices.t()
+    # Extract values at the non-zero indices
+    values = x[tuple(indices[i] for i in range(indices.size(0)))]
+    # Create a sparse tensor with the same type as the input tensor
+    return torch.sparse_coo_tensor(indices, values, x.size()).to(x.dtype)
+    
 def cal_adj(data, parameter):
     """Calculate the adjacency matrix"""
     dist = cosine_dis(data, data)
@@ -7,6 +27,10 @@ def cal_adj(data, parameter):
     adj = F.normalize(adj + torch.eye(adj.shape[0]), p=1)
     return to_sparse(adj)
 
+def cal_adj_parameter(edge_per_node, data):
+    dist = cosine_dis(data, data)
+    parameter = torch.sort(dist.reshape(-1,)).values[edge_per_node*data.shape[0]]
+    return parameter.data.cpu().numpy().item()
 
 def cal_te_adj(data_all, data_train_val, data_test, train_val_indices, test_indices, threshold):
     num_train_val = len(train_val_indices)
@@ -41,20 +65,5 @@ def sym_norm(adjacency):
     adjacency = adjacency + adj_T * (adj_T > adjacency).float() - adjacency * (adj_T > adjacency).float()
     return F.normalize(adjacency + identity, p=1)
 
-def to_sparse(x):
-    # Check if the tensor is already sparse
-    if x.is_sparse:
-        return x
-    # Find non-zero indices
-    indices = torch.nonzero(x, as_tuple=False)
-    # Handle the case where all elements are zeros
-    if indices.numel() == 0:
-        return torch.sparse_coo_tensor([], [], x.size()).to(x.dtype)
 
-    # Transpose the indices to match sparse tensor format
-    indices = indices.t()
-    # Extract values at the non-zero indices
-    values = x[tuple(indices[i] for i in range(indices.size(0)))]
-    # Create a sparse tensor with the same type as the input tensor
-    return torch.sparse_coo_tensor(indices, values, x.size()).to(x.dtype)
 
