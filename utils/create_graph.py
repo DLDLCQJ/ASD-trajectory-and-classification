78c989eb5c5465c5b6ee1b769514552147902765
Simon_Wang <simon971234@gmail.com>
2024-05-10 15:28:20 +0800

    Update create_graph.py

--- utils/create_graph.py
+++ utils/create_graph.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def create_graph(dist, parameter, self_dist=True):
     if self_dist:
         assert dist.shape[0] == dist.shape[1], "Input must be a square matrix for self distances."
